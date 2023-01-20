b83e179ce766996b630e15c072242d56d6c7e6ce
minnsane <minnsane.kim@gmail.com>
2023-01-21 00:04:35 +0900

    feat: 정적 이미지 파일 전시

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hello World!</h1>
+    <img src="/assets/images/basic-shield.png" />`,
 })
 export class AppComponent {
   title = 'ng-fundamentals';

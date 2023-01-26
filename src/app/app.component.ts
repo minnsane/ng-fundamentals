fdfa39650b00eb4f0cc80be8fe7d69aa3a6dde29
minnsane <minnsane.kim@gmail.com>
2023-01-26 14:43:51 +0900

    feat: 단방향 바인딩 적용

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>
-    <img src="/assets/images/basic-shield.png" />`,
+  template: `<events-list></events-list>`,
 })
 export class AppComponent {
   title = 'ng-fundamentals';

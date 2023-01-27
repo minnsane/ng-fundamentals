6d2f3b44a908bced37c0588f9000e12e4d920774
minnsane <minnsane.kim@gmail.com>
2023-01-27 14:34:07 +0900

    feat: 부모->자식 바인딩 Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
-  declarations: [AppComponent, EventsListComponent],
+  declarations: [AppComponent, EventsListComponent, EventThumbnailComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],

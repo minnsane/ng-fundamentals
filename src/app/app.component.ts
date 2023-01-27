ec241cc4dd7b271cb53c4e4ae06aaf377d43e907
minnsane <minnsane.kim@gmail.com>
2023-01-27 15:26:12 +0900

    feat: navbar 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<events-list></events-list>`,
+  template: `<nav-bar></nav-bar><events-list></events-list>`,
 })
 export class AppComponent {
   title = 'ng-fundamentals';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, EventsListComponent, EventThumbnailComponent],
+  declarations: [
+    AppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],

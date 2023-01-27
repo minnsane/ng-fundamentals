09353b3cee1e414de709cded58cfd496c4fb40c2
minnsane <minnsane.kim@gmail.com>
2023-01-27 15:16:37 +0900

    chore: css encapsulation

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
     <div>
       <h1>Upcoming Angular Events</h1>
       <hr />
+      <div class="well"><div>Hello World!</div></div>
       <event-thumbnail [event]="event1"></event-thumbnail>
     </div>
   `,

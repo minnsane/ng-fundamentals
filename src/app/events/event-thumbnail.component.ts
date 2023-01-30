6f10ba2ab1053fc043678f5a280b470ca8e950eb
minnsane <minnsane.kim@gmail.com>
2023-01-30 13:48:38 +0900

    feat: 클래스 바인딩으로 single class 바인드

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,12 @@ import { Component, Input } from '@angular/core';
     <div class="well hoverwell thumbnail">
       <h2>{{ event?.name }}</h2>
       <div>Date: {{ event?.date }}</div>
-      <div>Time: {{ event?.time }}</div>
+      <div [ngSwitch]="event?.time" [class.green]="event?.time === '8:00 am'">
+        Time: {{ event?.time }} (
+        <span *ngSwitchCase="'8:00 am'">Early Start</span>
+        <span *ngSwitchCase="'10:00 am'">Late Start</span>
+        <span *ngSwitchDefault>Normal Start</span>)
+      </div>
       <div>Price: \${{ event?.price }}</div>
       <div *ngIf="event?.location">
         <span>Location: {{ event?.location?.address }}</span>
@@ -29,6 +34,10 @@ import { Component, Input } from '@angular/core';
 
       .well div {
         color: #bbb;
+      }
+
+      .green {
+        color: green !important;
       }
     `,
   ],
